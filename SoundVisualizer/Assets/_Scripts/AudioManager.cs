ee3c6029a4aa2b3a60f1759607edad24863c80d9
asmongold <47479070+asmongold@users.noreply.github.com>
2019-02-16 20:08:19 +0100

    Split Samples to Frequency bands

--- SoundVisualizer/Assets/_Scripts/AudioManager.cs
+++ SoundVisualizer/Assets/_Scripts/AudioManager.cs
@@ -5,7 +5,7 @@ public class AudioManager : MonoBehaviour
 {
     AudioSource audioSource;
     public static float[] samples = new float[512];
-    
+    public static float[] freqBands = new float[8];
 
     void Start()
     {
@@ -16,11 +16,39 @@ public class AudioManager : MonoBehaviour
     void Update()
     {
         GetSpectrumAudioSource();
+        CreateFrequencyBands();
     }
 
 
     void GetSpectrumAudioSource()
     {
         audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void CreateFrequencyBands()
+    {
+        // 22050Hz / 512 Samples = 43Hz per Sample
+
+        int count = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if (i == 7) 
+            {
+                sampleCount += 2;
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count] * (count + 1);
+                count++;
+                
+            }
+
+            average /= count;
+            freqBands[i] = average * 10;
+        }
     }
 }
---
+++ SoundVisualizer/Assets/_Scripts/ParamCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour
+{
+    public int band;
+    public float startScale;
+    public float scaleLevel;
+
+    void Start()
+    {
+       
+    }
+
+   
+    void Update()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, (AudioManager.freqBands[band] * scaleLevel) + startScale, transform.localScale.z);
+    }
+}
